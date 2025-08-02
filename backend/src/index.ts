177e96822b6fe804d48abcfedef57d6e88d5d9fa
henrydaoo <henrydaoteam@gmail.com>
2025-08-02 15:51:35 +0700

    feat:[BE] invite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import workspaceRoutes from "./routes/workspace.route";
+import memberRoutes from "./routes/member.route";
 
 
 const BASE_PATH = config.BASE_PATH;
@@ -57,6 +58,7 @@ app.get(
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 
 
 app.use(errorHandler);

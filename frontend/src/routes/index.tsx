0acfddfc9a3d3dd706550111510a690aacf346bb
henrydaoo <henrydaoteam@gmail.com>
2025-08-02 12:03:54 +0700

    feat:[FE] auth page

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,53 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AppLayout from "@/layout/app.layout";
+import BaseLayout from "@/layout/base.layout";
+import AuthRoute from "./auth.route";
+import {
+  authenticationRoutePaths,
+  baseRoutePaths,
+  protectedRoutePaths,
+} from "./common/routes";
+import ProtectedRoute from "./protected.route";
+import NotFound from "@/page/errors/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<BaseLayout />}>
+          {baseRoutePaths.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+
+        <Route path="/" element={<AuthRoute />}>
+          <Route element={<BaseLayout />}>
+            {authenticationRoutePaths.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Route>
+        </Route>
+
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            {protectedRoutePaths.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;

aa173788604018a4c481f504f8dea598015a79da
henrydaoo <henrydaoteam@gmail.com>
2025-08-05 19:56:21 +0700

    feat (FE) manage animation play state

--- frontend/src/page/home/HomePage.tsx
+++ frontend/src/page/home/HomePage.tsx
@@ -3,10 +3,17 @@ import CTA from "@/components/home/cta";
 import Header from "@/components/home/header";
 import HeroSection from "@/components/home/hero";
 import Footer from "@/components/home/footer";
+import { usePageVisibility } from "@/hooks/use-page-visibility";
 
 const HomePage = () => {
+  const { hasBeenVisible } = usePageVisibility();
+
   return (
-    <div className="font-sans antialiased bg-[#084FFF] relative min-h-screen flex flex-col">
+    <div
+      className={`font-sans antialiased bg-[#084FFF] relative min-h-screen flex flex-col ${
+        hasBeenVisible ? "page-visible" : ""
+      }`}
+    >
       <Background />
       <div className="relative z-10 flex flex-col min-h-screen">
         <Header />

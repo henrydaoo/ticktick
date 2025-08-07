0863eb6d151d3bf2dd7c682541b8b4985125345f
henrydaoo <henrydaoteam@gmail.com>
2025-08-07 21:00:47 +0700

    feat (FE) improve performance

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,29 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import AppLayout from "@/layout/app.layout";
-import BaseLayout from "@/layout/base.layout";
-import AuthRoute from "./auth.route";
+const AppLayout = lazy(() => import("@/layout/app.layout"));
+const BaseLayout = lazy(() => import("@/layout/base.layout"));
+const AuthRoute = lazy(() => import("./auth.route"));
+const ProtectedRoute = lazy(() => import("./protected.route"));
+const NotFound = lazy(() => import("@/page/errors/NotFound"));
 import {
   authenticationRoutePaths,
   baseRoutePaths,
   protectedRoutePaths,
 } from "./common/routes";
-import ProtectedRoute from "./protected.route";
-import NotFound from "@/page/errors/NotFound";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<BaseLayout />}>
-          {baseRoutePaths.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-        </Route>
-
-        <Route path="/" element={<AuthRoute />}>
+      <Suspense
+        fallback={
+          <div className="w-full h-screen flex items-center justify-center">
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
           <Route element={<BaseLayout />}>
-            {authenticationRoutePaths.map((route) => (
+            {baseRoutePaths.map((route) => (
               <Route
                 key={route.path}
                 path={route.path}
@@ -31,21 +32,33 @@ function AppRoutes() {
               />
             ))}
           </Route>
-        </Route>
 
-        <Route path="/" element={<ProtectedRoute />}>
-          <Route element={<AppLayout />}>
-            {protectedRoutePaths.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
+          <Route path="/" element={<AuthRoute />}>
+            <Route element={<BaseLayout />}>
+              {authenticationRoutePaths.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route element={<AppLayout />}>
+              {protectedRoutePaths.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
